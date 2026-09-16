fe6fa9866a5ffadf4cf40451bd6d1829bdcdac80
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2026-09-16 11:14:44 +0200

    Restore default WordPress widgets
    This change removes the theme's unregister calls for the default core widgets. The site will now keep WordPress' built-in widgets available instead of disabling them during widget initialization.

--- includes/widgets.php
+++ includes/widgets.php
@@ -10,21 +10,6 @@ require_once get_template_directory() . '/includes/widgets/orbis-widget-stats.ph
  */
 function orbis_widgets_init() {
 
-	/* Unregister default WordPress Widgets */
-
-	unregister_widget( 'WP_Widget_Pages' );
-	unregister_widget( 'WP_Widget_Calendar' );
-	unregister_widget( 'WP_Widget_Archives' );
-	unregister_widget( 'WP_Widget_Links' );
-	unregister_widget( 'WP_Widget_Meta' );
-	unregister_widget( 'WP_Widget_Search' );
-	unregister_widget( 'WP_Widget_Categories' );
-	unregister_widget( 'WP_Widget_Recent_Posts' );
-	unregister_widget( 'WP_Widget_RSS' );
-	unregister_widget( 'WP_Widget_Tag_Cloud' );
-	unregister_widget( 'WP_Nav_Menu_Widget' );
-	unregister_widget( 'WP_Widget_Recent_Comments' );
-
 	/* Register custom WordPress Widgets */
 
 	register_widget( 'Orbis_Stats_Widget' );
