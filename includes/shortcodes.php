a049c2e2aa53343bccaebff5d4ce3d97734917e3
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2026-09-16 12:15:54 +0200

    Remove row and col shortcodes
    Drop the custom Bootstrap grid shortcodes from `includes/shortcodes.php`, leaving only the shortcode paragraph cleanup filter. This removes support for the `[row]` and `[col]` wrappers that generated `row` and `col-md-*` markup.
    
    Closes https://github.com/pronamic/wt-orbis-5/issues/10

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -16,27 +16,3 @@ function orbis_shortcode_empty_paragraph_fix( $content ) {
 }
 
 add_filter( 'the_content', 'orbis_shortcode_empty_paragraph_fix' );
-
-/**
- * Grid
- */
-function orbis_row_grid( $atts, $content = null ) {
-	$output = '<div class="row">' . do_shortcode( $content ) . '</div>';
-
-	return $output;
-}
-
-function orbis_columns_grid( $atts, $content = null ) {
-	$atts = shortcode_atts(
-		[
-			'number' => '12',
-			'offset' => '',
-		],
-		$atts
-	);
-
-	return '<div class="col-md-' . $atts['number'] . ' ' . ( $atts['offset'] ? 'col-md-offset-' . $atts['offset'] : '' ) . '">' . do_shortcode( $content ) . '</div>';
-}
-
-add_shortcode( 'row', 'orbis_row_grid' );
-add_shortcode( 'col', 'orbis_columns_grid' );
